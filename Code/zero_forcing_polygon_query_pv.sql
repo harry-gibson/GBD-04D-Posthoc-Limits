349e708d15c626d04d35b2e4f4c8c53fab3c7467
harry-gibson <harry.s.gibson@gmail.com>
2018-05-16 13:48:57 +0100

    added Pv workbench/SQL (have already been run) to repo

---
+++ Code/zero_forcing_polygon_query_pv.sql
@@ -0,0 +1,9 @@
+SELECT
+  pv_mask.area_id,
+  pv_mask.year,
+  areas.area_dset_code,
+  ST_AsEWKB(areas.area_multi_polygon) geom,
+  areas.area_name
+FROM staging.gbd_zero_avi_mask pv_mask
+  JOIN rds.area_insts areas ON pv_mask.area_id = areas.area_id
+ORDER BY area_id, year;
